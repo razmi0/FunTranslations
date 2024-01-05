d008a757e4433af755e8b27e7733b03f27b76c75
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-05 20:45:58 +0100

    Refactor translation functionality and add master selection

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,10 @@
 import { createSignal } from "solid-js";
+import { masters } from "../data";
 import Translation from "./Translation";
 
-const url = "https://api.funtranslations.com/translate/yoda.json?text=How%20do%20you%20suck%20so%20hard%20at%20this";
+const baseUrl = "https://api.funtranslations.com/translate/";
+const defaultMaster = "yoda";
+const defaultText = "How do you suck so hard at this";
 type FunTranslationResponse =
   | ({
       success: {
@@ -21,17 +24,40 @@ type MainProps = {
 
 const Main = (props: MainProps) => {
   const [result, setResult] = createSignal<string>("");
+  const [master, setMaster] = createSignal<string>(defaultMaster);
 
-  const handleCLick = async () => {
+  const urlifyText = (text: string) => {
+    return text.replace(/\s/g, "%20");
+  };
+
+  const handleSelectMaster = (e: Event) => {
+    const master = (e.target as HTMLInputElement).value;
+    setMaster(master);
+  };
+
+  const handleCLick = async (master: string, text: string = defaultText) => {
+    const url = `${baseUrl}${master}?text=${urlifyText(text)}`;
     const response = await fetch(url);
     const data: FunTranslationResponse = await response.json();
+    console.log(data);
     setResult(data?.contents.translated || "");
   };
 
   return (
     <main class="main">
-      <h2 class="pb-3">{props.title}</h2>
-      <Translation handleCLick={handleCLick} type="yoda" result={result()} />
+      <div class="my-3">
+        <h2 class="inline">{props.title} : </h2>
+        <select name="master" id="master" class="text-gray-900" onInput={(e) => handleSelectMaster(e)}>
+          <option disabled selected>
+            Select a master
+          </option>
+          {masters.map((master) => (
+            <option value={master}>{master}</option>
+          ))}
+        </select>
+      </div>
+
+      <Translation handleCLick={handleCLick} result={result()} master={master()} />
     </main>
   );
 };
--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,23 +1,39 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 
 type TranslationProps = {
-  handleCLick: () => void;
-  type: string; // MasterType (yoda, pirate, etc.)
+  handleCLick: (master: string, text: string) => void;
   result: string;
+  master: string;
 };
 const Translation = (props: TranslationProps) => {
+  const [text, setText] = createSignal<string>("");
+
+  const handleCLick = () => {
+    props.handleCLick(props.master, text());
+  };
+
+  const handleTextChange = (e: Event) => {
+    setText((e.target as HTMLInputElement).value);
+  };
+
   return (
     <div class="flex flex-col">
       <div class="flex flex-row">
-        <div class="main-box text-5xl">{props.type.toLocaleUpperCase()}</div>
+        <div class="main-box text-5xl">{props.master.toLocaleUpperCase()}</div>
         <div class="main-box">
-          <input type="text" class="input w-full mr-2" name="text" placeholder="Text to translate" />
+          <input
+            type="text"
+            class="input w-full mr-2"
+            name="text"
+            placeholder="Text to translate"
+            onInput={(e) => handleTextChange(e)}
+          />
         </div>
-        <button onClick={props.handleCLick}>
+        <button onClick={handleCLick}>
           <span>Translate</span>
         </button>
       </div>
-      <output class="w-full border min-h-10">{props.result}</output>
+      <output class="w-full min-h-10">{props.result}</output>
     </div>
   );
 };
