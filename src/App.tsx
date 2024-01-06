76d327072eab0b61fd2b295504fce8caeb420cb8
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-07 00:50:26 +0100

    Remove Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import type { Component } from "solid-js";
-import Header from "./components/Header";
 import Main from "./components/Main";
 
-const headerText = ["Welcome to Translator", "The only purpose is to do fun translation", "Fun translation is fun"];
 const mainTitle = "Translations from another world";
 
 const App: Component = () => {
   return (
     <>
       <div class="container flex flex-col h-full">
-        <Header segOne={headerText[0]} segTwo={headerText[1]} segThree={headerText[2]} />
         <Main title={mainTitle} />
       </div>
-      <footer class="footer border">🌈🍩 & code & coffee</footer>
+      <footer class="footer border">🌈🍩 coffeeCode</footer>
     </>
   );
 };
