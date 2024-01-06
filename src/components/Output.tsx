712317e2d50d2afe3c1d905a0ee51db8a95274ad
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-07 00:54:08 +0100

    Moved emoji logic in Main & three dots anim

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,40 +1,22 @@
 import { Show } from "solid-js";
-import type { Resource } from "solid-js";
+import type { Resource, VoidComponent } from "solid-js";
 
-/**
- * Converts unicode to emoji by removing "&#x" and ";" and converting the remaining hex to a code point
- * &#x1F44D; -> 👍
- */
-const convertUniToEmoji = (unicode: string) => {
-  const hex = unicode.slice(3, -1); // Remove "&#x" and ";"
-  return String.fromCodePoint(parseInt(hex, 16));
+type OutputProps = {
+  result: Resource<FunTranslationResponse["contents"] | undefined>;
 };
-
-const convertStrIfEmoji = (str: string) => {
-  const regex = /&#x[0-9A-Fa-f]+;/g;
-  const matches = str.match(regex) || [];
-  const treated = str.replace(regex, convertUniToEmoji(matches[0] || ""));
-  return treated;
-};
-
-const hasEmoji = (text: string): boolean => {
-  const regex = /&#x[0-9A-Fa-f]+;/g;
-  const matches = text.match(regex) || [];
-  return !!matches[0];
-};
-
-const Output = (props: { result: Resource<string | undefined> }) => {
+const Output: VoidComponent<OutputProps> = (props) => {
   return (
     <>
       <Show when={props.result.loading}>
-        <div class="w-full min-h-10">Loading...</div>
+        <div class="w-full min-h-10 three-points-animation">Loading</div>
       </Show>
-      <Show when={props.result.error}>
-        <div class="w-full min-h-10">No 💋 : {props.result.error}</div>
+      <Show when={!!props.result.error}>
+        <div class="w-full min-h-10">No 💋 </div>
       </Show>
       <Show when={props.result()}>
         <output class="w-full min-h-10">
-          {hasEmoji(props.result() as string) ? convertStrIfEmoji(props.result() as string) : props.result()}
+          {"> "}
+          {props.result()?.translated}
         </output>
       </Show>
     </>
