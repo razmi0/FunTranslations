dad053166ad1e8d8b31e5b4012ca8d653246ba21
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-07 02:56:47 +0100

    Add components, externalize async logic, add random signals

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,45 +1,39 @@
-import { createSignal, createResource, createEffect, For, Show, onMount } from "solid-js";
+import { createSignal, createResource, createEffect, ParentComponent } from "solid-js";
 import { createStore, produce } from "solid-js/store";
 import Select from "./Select";
 import Translation from "./Translation";
 import History from "./History";
-import { masters } from "../data";
-import { emojize, hasEmoji, urlifyText } from "../helpers";
+import RandomButton from "./RandomButton";
+import { masters, sentences } from "../data";
+import { urlifyText } from "../helpers";
+import { fetchTranslation } from "../services/translation";
+import Output from "./Output";
 
-const baseUrl = "https://api.funtranslations.com/translate/";
-const defaultMaster = "Yoda";
-const defaultText = "How do you suck so hard at this";
-
-const fetchTranslation = async (url: string) => {
-  const response = await fetch(url);
-  const data: FunTranslationResponse = await response.json();
-
-  if (hasEmoji(data.contents.translated)) {
-    data.contents.translated = emojize(data.contents.translated);
-  }
-  return data.contents as FunTranslationResponse["contents"];
-};
-
-const buildUrl = (master: string = defaultMaster, text: string = defaultText) => {
-  return `${baseUrl}${master}?text=${urlifyText(text)}`;
+const buildUrlParam = (master: string = masters[0], text: string = sentences[0]) => {
+  return `${master}?text=${urlifyText(text)}`;
 };
 
 const Main = (props: MainProps) => {
-  const [url, setUrl] = createSignal<string>();
-  const [master, setMaster] = createSignal<Master>(defaultMaster);
-  const [content /*{ mutate, refetch }*/] = createResource(url, fetchTranslation);
+  const [urlParam, setUrlParam] = createSignal<string>();
+  const [defaultMaster, setDefaultMaster] = createSignal<Master>("Yoda");
+  const [master, setMaster] = createSignal<Master>(masters[0]);
+  const [randomSentence, setRandomSentence] = createSignal<string>(sentences[0]);
+  const [content /*{ mutate, refetch }*/] = createResource(urlParam, fetchTranslation);
   const [history, setHistory] = createStore<HistoryType>({ past: [] });
 
-  const buildAndSetUrl = (master: Master, text: string) => {
-    setUrl(buildUrl(master, text));
+  const buildAndSetUrlParam = (master: Master, text: string) => {
+    setUrlParam(buildUrlParam(master, text));
+  };
+
+  const Heading: ParentComponent = (props) => {
+    return <div class="main-box text-5xl">{props.children}</div>;
   };
 
   createEffect(() => {
-    const contentValue = content();
-    if (contentValue) {
+    if (content()) {
       setHistory(
         produce((draft) => {
-          if (draft && Array.isArray(draft.past)) draft.past.push(contentValue);
+          if (draft && Array.isArray(draft.past)) draft.past.push(content()!);
         })
       );
     }
@@ -47,11 +41,22 @@ const Main = (props: MainProps) => {
 
   return (
     <main class="main">
-      <div class="my-3">
+      <div class="my-3 flex gap-2">
         <h2 class="inline">{props.title} : </h2>
-        <Select masters={masters} defaultMaster={defaultMaster as Master} onSelected={setMaster} />
+        <Select masters={masters} defaultMaster={defaultMaster()} onSelected={setMaster} />
+        <RandomButton
+          masters={masters}
+          setMaster={setMaster}
+          setDefaultMaster={setDefaultMaster}
+          setSentence={setRandomSentence}
+        >
+          Random
+        </RandomButton>
       </div>
-      <Translation handleClick={buildAndSetUrl} result={content} master={master()} />
+      <Translation handleClick={buildAndSetUrlParam} master={master()} randomSentence={randomSentence()}>
+        <Heading>{master().toLocaleUpperCase()}</Heading>
+      </Translation>
+      <Output result={content} />
       <History history={history} />
     </main>
   );
