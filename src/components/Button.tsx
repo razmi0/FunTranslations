08e2ab0b70f5dc7da30204697658af85c51afff5
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-10 01:24:49 +0100

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+  classes?: string;
+  onClick: (args: any) => void;
+  children: string;
+};
+const Button = (props: ButtonProps) => {
+  return (
+    <button onClick={props.onClick} class={props.classes}>
+      <span>{props.children}</span>
+    </button>
+  );
+};
+
+export default Button;
