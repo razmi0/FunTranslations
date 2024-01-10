07ce074ed670cf4f5afb17c5c6d2d25f9cbec24c
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-10 01:34:27 +0100

    *

--- src/services/translation.ts
+++ src/services/translation.ts
@@ -1,14 +1,15 @@
+import { createSignal } from "solid-js";
 import { emojize, hasEmoji } from "../helpers";
 
 const baseUrl = "https://api.funtranslations.com/translate/";
 
-export const fetchTranslation = async (url: string) => {
-  const response = await fetch(baseUrl + url);
+export const fetchTranslation = async (urlParams: string) => {
+  const response = await fetch(baseUrl + urlParams);
   const data: FunTranslationResponse = await response.json();
+  console.log("fetch");
 
-  if (hasEmoji(data.contents.translated)) {
+  if (data.contents && hasEmoji(data.contents.translated)) {
     data.contents.translated = emojize(data.contents.translated);
   }
-
-  return data.contents as FunTranslationResponse["contents"];
+  return data.contents as ContentType;
 };
