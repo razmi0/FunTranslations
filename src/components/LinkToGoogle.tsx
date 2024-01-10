7149a5a1c2b69395bda39ba9f724e0a51e306968
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-10 23:16:04 +0100

    *

--- src/components/LinkToGoogle.tsx
+++ src/components/LinkToGoogle.tsx
@@ -3,12 +3,14 @@ import { Component } from "solid-js";
 type LinkToGoogleProps = {
   children: string;
   searchParam?: string;
+  classes: string;
 };
 const LinkToGoogle: Component<LinkToGoogleProps> = (props) => {
   return (
     <a
       href={`https://www.google.com/search?q=${props.searchParam ? props.searchParam : props.children}`}
       target="_blank"
+      class={props.classes}
     >
       <span class="translated-text">{props.children}</span>
     </a>
